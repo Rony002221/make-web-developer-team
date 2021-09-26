2620f7d12bd6deed75c6437a062b696ce59a9545
rony <rony49883@gmail.com>
2021-09-26 11:22:36 +0600

    add Header Componet

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className = "header_Section">
+            <h1>Make A Web Developer Team</h1>
+            <h4>Let's to make the best team for your new start-up software company</h4>
+            <h2>Total Budget : <span><i>50 Millions</i></span></h2>
+        </div>
+    );
+};
+
+export default Header;
